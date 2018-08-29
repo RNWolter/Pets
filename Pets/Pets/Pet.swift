6bf244bebc221e16f6e923e9458db8f31c3a75a3
Rwoltx <richardx1016@gmail.com>
2018-08-28 22:45:10 -0400

    Initial commit of Pets assignment.

---
+++ Pets/Pets/Pet.swift
@@ -0,0 +1,16 @@
+//
+//  Pet.swift
+//  Pets
+//
+//  Created by Rick Wolter on 8/28/18.
+//  Copyright © 2018 RNWolter. All rights reserved.
+//
+
+import Foundation
+
+struct Pet {
+    
+    var name: String
+    var type: String
+    
+}
